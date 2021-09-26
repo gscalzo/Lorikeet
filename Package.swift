54ff2477220cf58b23adcf5776808a26429a4a58
giordano scalzo <giordano.scalzo@gmail.com>
2021-09-26 18:38:40 +0100

    Added SPM support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Lorikeet",
-    path: "Lorikeet",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -21,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Lorikeet",
+            path: "Lorikeet",
             dependencies: []),
         .testTarget(
             name: "LorikeetTests",
